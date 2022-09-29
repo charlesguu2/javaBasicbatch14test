a8102b172fecb084acb6a78b001ba3eece0a7d67
user.email <charlesguevara273@gmail.com>
2022-09-28 21:20:35 -0400

    first java commit

---
+++ src/com/javaintro/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.javaintro;
+
+public class HelloWorld{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+                   
+	System.out.println("Hello Batch 14");
+	
+	System.out.println("20"); 
+	
+	System.out.println("My favorite color is Blue");
+   
+	         }
+	
+
+
+}
